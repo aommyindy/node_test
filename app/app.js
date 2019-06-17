525f415f5f884523526c73a3dc93d257a02e3482
aOmmy <sutumrongpun@bigc.co.th>
2019-06-17 13:43:06 +0700

    Server start!!

---
+++ app/app.js
@@ -0,0 +1,8 @@
+const http = require('http')
+
+http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'})
+    res.end('yes you can\n')
+}).listen(8000)
+
+console.log('Server start!!')
