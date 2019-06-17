3cd9b6df8907b42b4d6448508274cb5703d268be
aOmmy <sutumrongpun@bigc.co.th>
2019-06-17 13:30:35 +0700

    ThisWorld

--- app/service/exam1.js
+++ app/service/exam1.js
@@ -1,8 +1,15 @@
 class Exam1 {
+    constructor(var1) {
+        this.var1 = var1
+    }
+
     add(a, b) {
         return (a * 1000 + b * 1000)/1000
     }
+
+    helloWorldCallback(greeting) {
+        greeting('Hello', this.var1 + 'World')
+    }
 }
 
-const exam1 = new Exam1()
-module.exports = new Exam1()
+module.exports = new Exam1('This')
--- tests/exam1.spec.js
+++ tests/exam1.spec.js
@@ -10,4 +10,16 @@ describe('Exam1', () => {
         const data = exam1.add(1, 1.235)
         expect(data).toEqual(2.235)
     })
+
+    test('Should call helloWorldCallback retrun = hello', () => {
+        exam1.helloWorldCallback((h,w) => {
+            expect(h).toEqual('Hello')
+        })
+    })
+
+    test('Should call helloWorldCallback retrun = ThisWorld', () => {
+        exam1.helloWorldCallback((h,w) => {
+            expect(w).toEqual('ThisWorld')
+        })
+    })
 })
