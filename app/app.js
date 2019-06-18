181e22305535bc51e502fb5599a2ff49a080bc85
aOmmy <sutumrongpun@bigc.co.th>
2019-06-18 10:16:07 +0700

    bodyParser

--- app/app.js
+++ app/app.js
@@ -1,12 +1,27 @@
 const express = require('express')
 const cors = require('cors')
+const bodyParser = require('body-parser')
 
 const app = express()
+
 app.use(cors())
 app.options('*', cors())
 
+app.use(bodyParser.urlencoded({ extended: true}))
+// parse application/json
+app.use(bodyParser.json())
+
 app.get('/', (req, res) => {
     res.end('Hello World')
+})
+
+app.post('/', (req, res) => {
+    const data = req.body
+
+    res.json({
+        success: true,
+        data: data
+    })
 })
 
 module.exports = app
